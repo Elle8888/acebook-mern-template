683c8336c693afdc353380eb63b54348241e0fc2
Abbey <abbeyjeannieminett@gmail.com>
2023-01-10 15:03:22 +0000

    signup and login pages styled with working button

--- frontend/src/components/user/SignUpForm.js
+++ frontend/src/components/user/SignUpForm.js
@@ -35,11 +35,34 @@ const SignUpForm = ({ navigate }) => {
 
     return (
       <form onSubmit={handleSubmit}>
-          <input placeholder="Email" id="email" type='text' value={ email } onChange={handleEmailChange} />
-          <input placeholder="Password" id="password" type='password' value={ password } onChange={handlePasswordChange} />
-        <input id='submit' type="submit" value="Submit" />
+        <div className="box-form">
+          <div className="left">
+            <div className="overlay">
+              <h1>Fish Book</h1>
+            </div>
+          </div>
+          <div className="right">
+            <h5>Sign up!</h5>
+            <p>Already have an account? <a href="/login">Log into your account</a> it takes less than a minute!</p>
+            <div className="inputs">
+              <input placeholder='Email' id="email" type='text' value={email} onChange={handleEmailChange} required />
+
+                <input placeholder='Password' id="password" type='password' value={password} onChange={handlePasswordChange} required />
+
+            </div>
+          
+          <div className="remember-me--forget-password">
+            <label>
+              <input type="checkbox" name="item" checked />
+              <span className="text-checkbox">Remember me</span>
+            </label>
+            
+          <button role='submit-button' id='submit' type="submit" value="Submit">Signup</button>
+          </div>
+          </div>
+        </div>
       </form>
-    );
+  );
 }
 
 export default SignUpForm;
