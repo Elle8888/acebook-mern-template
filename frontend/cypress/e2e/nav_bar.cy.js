aae966ccc1093d497625e5128fc14f3884397135
Elle <lou.d.b.rigodon@gmail.com>
2023-01-10 15:52:03 +0100

    deleted spaces

--- frontend/cypress/e2e/nav_bar.cy.js
+++ frontend/cypress/e2e/nav_bar.cy.js
@@ -2,23 +2,22 @@ describe("Nav bar", () => {
     it("redirects to '/signup'", () => {
       cy.visit("/");
       cy.get("#signup-link-nav").click();
-  
+
       cy.url().should("include", "/signup");
     });
-  
+
     it("redirects to '/login'", () => {
         cy.visit("/");
         cy.get("#login-link-nav").click();
-    
+
         cy.url().should("include", "/login");
       });
 
     it("redirects to '/signup'", () => {
         cy.visit("/");
         cy.get("#posts-link-nav").click();
-    
+
         cy.url().should("include", "/signup");
     });
-
       // needs login testing
   });
