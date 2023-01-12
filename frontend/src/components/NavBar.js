3365cf61377f76cd4d14b54e81fe34ea07b2baa1
Elle <lou.d.b.rigodon@gmail.com>
2023-01-12 23:54:47 +0100

    add logout link

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,25 +1,36 @@
-import { Link, useMatch, useResolvedPath } from "react-router-dom"
+import { Link, useMatch, useResolvedPath } from 'react-router-dom'
 export default function Navbar() {
-  return <nav className="nav">
-    <Link to="/" className="site-title">Fishbook
+  return (
+    <nav className="nav">
+      <Link to="/" className="site-title">
+        Fishbook
       </Link>
-        <ul>
-          <CustomLink id="posts-link-nav" to="/posts">Post</CustomLink>
-          <CustomLink id="login-link-nav" to="/login">Login</CustomLink>
-          <CustomLink id="signup-link-nav" to="/signup">Signup</CustomLink>
-          <CustomLink id="logout-link-nav" to="/login">Logout</CustomLink>
-        </ul>
+      <ul>
+        <CustomLink id="posts-link-nav" to="/posts">
+          Post
+        </CustomLink>
+        <CustomLink id="login-link-nav" to="/login">
+          Login
+        </CustomLink>
+        <CustomLink id="signup-link-nav" to="/signup">
+          Signup
+        </CustomLink>
+        <CustomLink id="logout-link-nav" to="/logout">
+          Logout
+        </CustomLink>
+      </ul>
     </nav>
+  )
 }
 
 function CustomLink({ to, children, ...props }) {
   const resolvedPath = useResolvedPath(to)
-  const isActive = useMatch({ path: resolvedPath.pathname, end:true })
+  const isActive = useMatch({ path: resolvedPath.pathname, end: true })
   return (
-    <li className={isActive === to ? "active" : ""}>
+    <li className={isActive === to ? 'active' : ''}>
       <Link to={to} {...props}>
         {children}
       </Link>
     </li>
-    )
+  )
 }
