1f1593f7d84c505b291ad32a2054a81b11c7820c
Elle <lou.d.b.rigodon@gmail.com>
2023-01-12 14:34:11 +0100

    add tests

--- frontend/src/components/user/SignUpForm.cy.js
+++ frontend/src/components/user/SignUpForm.cy.js
@@ -1,17 +1,31 @@
 import SignUpForm from './SignUpForm'
 const navigate = () => {}
 
-describe("Signing up", () => {
-  it("calls the /users endpoint", () => {
-    cy.mount(<SignUpForm navigate={navigate}/>)
+describe('Signing up', () => {
+  it('calls the /users endpoint', () => {
+    cy.mount(<SignUpForm navigate={navigate} />)
 
-    cy.intercept('POST', '/users', { message: "OK" }).as("signUpRequest")
+    cy.intercept('POST', '/users', { message: 'OK' }).as('signUpRequest')
 
-    cy.get("#email").type("someone@example.com");
-    cy.get("#password").type("password");
-    cy.get("#submit").click();
-    cy.wait('@signUpRequest').then( interception => {
-      expect(interception.response.body.message).to.eq("OK")
+    cy.get('#email').type('someone@example.com')
+    cy.get('#password').type('password')
+    cy.get('#submit').click()
+    cy.wait('@signUpRequest').then((interception) => {
+      expect(interception.response.body.message).to.eq('OK')
     })
   })
+
+  it('requires a valid email address', () => {})
+
+  it('requires a password with 8 characters', () => {})
+
+  it('requires a password with one uppercase letter', () => {})
+
+  it('requires a password with one lowercase letter', () => {})
+
+  it('requires a password with one special character', () => {})
+
+  it('requires a password with one number', () => {})
+
+  it('redirects to login for successful signup', () => {})
 })
