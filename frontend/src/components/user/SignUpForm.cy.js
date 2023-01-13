a35e2fa43ce309bc9c56198aab5aaf91129cea32
Elle <lou.d.b.rigodon@gmail.com>
2023-01-13 11:23:55 +0100

    modify sign up

--- frontend/src/components/user/SignUpForm.cy.js
+++ frontend/src/components/user/SignUpForm.cy.js
@@ -1,31 +1,143 @@
+/* eslint-disable no-unused-expressions */
 import SignUpForm from './SignUpForm.js'
 const navigate = () => {}
 
-describe('Signing up', () => {
-  it('calls the /users endpoint', () => {
-    cy.mount(<SignUpForm navigate={navigate} />)
+// describe('Signing up', () => {
 
-    cy.intercept('POST', '/users', { message: 'OK' }).as('signUpRequest')
+//   // before(() => {
+//   //   // eslint-disable-next-line no-labels
+//   //   cy.mount(<SignUpForm navigate={navigate} />)
+//   //   cy.intercept('POST', '/users', { message: 'OK' }).as('signUpRequest')
+//   // })
 
-    cy.get('#email').type('someone@example.com')
-    cy.get('#password').type('password')
-    cy.get('#submit').click()
-    cy.wait('@signUpRequest').then((interception) => {
-      expect(interception.response.body.message).to.eq('OK')
-    })
+//   it('calls the /users endpoint', () => {
+//     cy.mount(<SignUpForm navigate={navigate} />)
+
+//     cy.intercept('POST', '/users', { message: 'OK' }).as('signUpRequest')
+
+//     cy.get('#email').type('test@test.com')
+//     cy.get('#password').type('12345678Ab*')
+//     cy.get('#submit').click()
+//     cy.wait('@signUpRequest').then((interception) => {
+//       expect(interception.response.body.message).to.eq('OK')
+//     })
+//   })
+
+
+//   it('requires a valid email address', () => {
+//     cy.mount(<SignUpForm navigate={navigate} />)
+//     cy.intercept('POST', '/users', { message: 'OK' }).as('signUpRequest')
+//     cy.get('#email').type('test')
+//     cy.get('#password').type('12345678Ab*')
+//     cy.get('#submit').click()
+//     cy.wait('@signUpRequest').then((interception) => {
+//       expect(interception.response.body.message).to.eq('Enter a valid email')
+//     })
+//   })
+
+//   it('requires a password with 8 characters', () => {
+//     cy.mount(<SignUpForm navigate={navigate} />)
+//     cy.intercept('POST', '/users', { message: 'OK' }).as('signUpRequest')
+//     cy.get('#email').type('test@test.com')
+//     cy.get('#password').type('Ab*')
+//     cy.get('#submit').click()
+//     cy.wait('@signUpRequest').then((interception) => {
+//       expect(interception.response.body.message).to.eq('Password must be at least 8 characters, with at least one uppercase & lowercase letters, character and number')
+//     })
+//   })
+
+//   it('requires a password with one uppercase letter', () => {
+//     cy.mount(<SignUpForm navigate={navigate} />)
+//     cy.intercept('POST', '/users', { message: 'OK' }).as('signUpRequest')
+//     cy.get('#email').type('test@test.com')
+//     cy.get('#password').type('12345678b*')
+//     cy.get('#submit').click()
+//     cy.wait('@signUpRequest').then((interception) => {
+//       expect(interception.response.body.message).to.eq('Password must be at least 8 characters, with at least one uppercase & lowercase letters, character and number')
+//     })
+//   })
+
+//   it('requires a password with one lowercase letter', () => {
+//     cy.mount(<SignUpForm navigate={navigate} />)
+//     cy.intercept('POST', '/users', { message: 'OK' }).as('signUpRequest')
+//     cy.get('#email').type('test@test.com')
+//     cy.get('#password').type('12345678A*')
+//     cy.get('#submit').click()
+//     cy.wait('@signUpRequest').then((interception) => {
+//       expect(interception.response.body.message).to.eq('Password must be at least 8 characters, with at least one uppercase & lowercase letters, character and number')
+//     })
+//   })
+
+//   it('requires a password with one special character', () => {
+//     cy.mount(<SignUpForm navigate={navigate} />)
+//     cy.intercept('POST', '/users', { message: 'OK' }).as('signUpRequest')
+//     cy.get('#email').type('test@test.com')
+//     cy.get('#password').type('12345678Ab')
+//     cy.get('#submit').click()
+//     cy.wait('@signUpRequest').then((interception) => {
+//       expect(interception.response.body.message).to.eq('Password must be at least 8 characters, with at least one uppercase & lowercase letters, character and number')
+//     })
+//   })
+
+//   it('requires a password with one number', () => {
+//     cy.mount(<SignUpForm navigate={navigate} />)
+//     cy.intercept('POST', '/users', { message: 'OK' }).as('signUpRequest')
+//     cy.get('#email').type('test@test.com')
+//     cy.get('#password').type('Ab*')
+//     cy.get('#submit').click()
+//     cy.wait('@signUpRequest').then((interception) => {
+//       expect(interception.response.body.message).to.eq('Password must be at least 8 characters, with at least one uppercase & lowercase letters, character and number')
+//     })
+//   })
+
+//   it('redirects to login for successful signup', () => {
+//     cy.mount(<SignUpForm navigate={navigate} />)
+//     cy.intercept('POST', '/users', { message: 'OK' }).as('signUpRequest')
+//     cy.get('#email').type('test@test.com')
+//     cy.get('#password').type('Ab*')
+//     cy.get('#submit').click()
+//     cy.url().should('include', '/posts')
+//   })
+// })
+
+describe('SignUpForm', () => {
+  beforeEach(() => {
+    cy.visit('/signup')
+    cy.get('#email').as('emailInput')
+    cy.get('#password').as('passwordInput')
+    cy.get('#submit').as('submitButton')
+  })
+
+  it('displays an error message when an invalid email is entered', () => {
+    cy.get('@emailInput').type('invalidemail')
+    cy.get('@passwordInput').type('validpassword')
+    cy.get('@submitButton').click()
+    cy.get('.error-msg').contains('Enter a valid email')
+  })
+
+  it('displays an error message when an invalid password is entered', () => {
+    cy.get('@emailInput').type('valid@email.com')
+    cy.get('@passwordInput').type('short')
+    cy.get('@submitButton').click()
+    cy.get('.error-msg').contains(
+      'Password must be at least 8 characters, with at least one uppercase & lowercase letters, character and number',
+    )
+  })
+
+  it('submits the form and redirects to the login page when valid email and password are entered', () => {
+    cy.server()
+    cy.route({
+      method: 'POST',
+      url: '/users',
+      status: 201,
+      response: {},
+    }).as('submitForm')
+
+    cy.get('@emailInput').type('valid@email.com')
+    cy.get('@passwordInput').type('validpassword')
+    cy.get('@submitButton').click()
+
+    cy.wait('@submitForm')
+    cy.url().should('include', '/login')
   })
 })
-
-// it('requires a valid email address', () => {})
-
-// it('requires a password with 8 characters', () => {})
-
-// it('requires a password with one uppercase letter', () => {})
-
-// it('requires a password with one lowercase letter', () => {})
-
-// it('requires a password with one special character', () => {})
-
-// it('requires a password with one number', () => {})
-
-// it('redirects to login for successful signup', () => {})
