893a8a45ada915aa2127c24ada97ef8504e5257c
Abigail Minett <106555227+AbbeyJM@users.noreply.github.com>
2023-01-17 16:54:57 +0000

    hyperlinks right colour

--- frontend/src/components/creators.js
+++ frontend/src/components/creators.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 export default function Creators() {
     return (
         <div className="pond">
+            <div className="creators">
             <h1>Meet the team!</h1>
 
             <h3><a href="https://github.com/HarryHibbins">Harry</a></h3>
@@ -13,7 +14,8 @@ export default function Creators() {
             <h3><a href="https://github.com/Elle8888">Elle</a></h3>
             <h3><a href="https://github.com/Mahasiddha95">Valerio</a></h3>
          
-            <h2>Thanks for visiting!</h2>
+                <h2>Thanks for visiting!</h2>
+            </div>
         </div>
     )
 }
