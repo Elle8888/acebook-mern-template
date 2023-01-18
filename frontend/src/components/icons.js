1a6412e2c2c30b16710354417878c760d1fdf0fe
Abigail Minett <106555227+AbbeyJM@users.noreply.github.com>
2023-01-18 15:00:53 +0000

    Michael made the icons clickable and set profile picture

--- frontend/src/components/icons.js
+++ frontend/src/components/icons.js
@@ -1,28 +1,20 @@
 import React, { useState } from 'react';
 
-export default function Icons() {
+
+export default function Icons({setSelectedFish, openFishSelector}) {
+    
+    const arr = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'el', 'twe', 'th', 'fo', 'fi', 'si', 'se', 'ei', 'ni', 'tw']
+
+    const chooseFishandClose = (el) => {
+        setSelectedFish(`${el}-icon`)
+        openFishSelector()
+    }
+
+    const allFish = arr.map((el) => <div className={`${el}-icon`} onClick={() => chooseFishandClose(`${el}`)}></div>)
+
     return ( 
         <div className="icons">
-            <div className="one-icon"></div>
-            <div className="two-icon"></div>
-            <div className="three-icon"></div>
-            <div className="four-icon"></div>
-            <div className="five-icon"></div>
-            <div className="six-icon"></div>
-            <div className="seven-icon"></div>
-            <div className="eight-icon"></div>
-            <div className="nine-icon"></div>
-            <div className="ten-icon"></div>
-            <div className="el-icon"></div>
-            <div className="twe-icon"></div>
-            <div className="th-icon"></div>
-            <div className="fo-icon"></div>
-            <div className="fi-icon"></div>
-            <div className="si-icon"></div>
-            <div className="se-icon"></div>
-            <div className="ei-icon"></div>
-            <div className="ni-icon"></div>
-            <div className="tw-icon"></div>
+            {allFish}
         </div>
     )
 }
